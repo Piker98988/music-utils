9d590fd1fc5918a3b3a6657b7c6f2c5f326afbf3
piker <piker.joestar98988@gmail.com>
2024-08-03 20:04:40 +0200

    cleaned up code

--- bulk-song-mover.sh
+++ bulk-song-mover.sh
@@ -1,29 +1,6 @@
 #! /bin/bash
-: "
-# move song
-echo "moving '$1' to music dir /mnt/dev150G/music/songs"
-mv "$1" /mnt/dev150G/music/songs
 
-# add to a playlist
-ls -l /mnt/dev150G/music/playlists
-read -e -p "select a playlist: " -i "change" playlist
-cp /mnt/dev150G/music/songs/"$1" /mnt/dev150G/music/playlists/"$playlist"
-
-printf "\n"
-# print directory so user knows it has been moved
-cd /mnt/dev150G/music/playlists/"$playlist" || exit
-pwd
-ls -ahl | grep "$1"
-
-
-cd /mnt/dev150G/music/songs || exit
-pwd
-ls -ahl | grep "$1"
-
-echo "complete."
-"
-
-
+# ask for files to be moved
 read -rep "Files to move: " -i '*' files_string
 
 # Create an array from the input string
@@ -31,12 +8,13 @@ readarray -t file_array <<< $files_string
 
 # Print the array to verify
 echo "Files to bulk move to the default music directory: (SONGS_DIR env var)"
-# shellcheck disable=SC2068
+
 for file in ${file_array[@]}
 do
     echo $file
 done
 
+# double check for confirmation of user
 while true; do
     read -p "Start the moving proccess for the aforementioned files? (y/n)" yn
     case $yn in
@@ -47,27 +25,21 @@ while true; do
 done
 
 # start the moving proccess
-# shellcheck disable=SC2068
 for file in ${file_array[@]}
 do
-    echo;
-#    new_filename=$(echo $file | sed 's/^[^ ]* [^ ]* //')
-#    echo $file"  ---->  "$new_filename
-#    mv "$file" "$new_filename"
-#    echo $file" renamed"
+    echo
     cp "$file" "$SONGS_DIR"
     echo "file "$file" has been succesfully moved to the default music directory."
 done
 
-# ask for files to be moved to a playlist, else, print files copied to $SONGS_DIR and remove the files from origin directory
-
+# ask for files to be moved to a playlist, else, print the songs copied to $SONGS_DIR and remove the files from origin directory
 while true; do
-    read -p "Would you like all of the music files to be moved to a playlist? (y/n) " yn
+    read -rp "Would you like all of the music files to be moved to a playlist? (y/n) " yn
     case $yn in
         [Yy]*) break;;
         [Nn]*)
+            # TODO: unclutter whatever is going on here
             echo "Moving successful. Files moved:"
-            # shellcheck disable=SC2068
             for file in ${file_array[@]}
             do
                 echo $file
@@ -79,28 +51,24 @@ while true; do
 done
 
 # list available playlists from $PLAYLISTS_DIR and ask for a playlist to move to
+echo "Available playlists in the PLAYLISTS_DIR env var:"
 ls -l "$PLAYLISTS_DIR"
-read -ep "select a playlist from the default playlist directory: " playlist
+read -rep "Select a playlist from the default playlist directory: " playlist
 
-# shellcheck disable=SC2068
+
+# move files from the origin dir to the target playlist dir
 for file in ${file_array[@]}
 do
-    echo;
-#    new_filename=$(echo $file | sed 's/^[^ ]* [^ ]* //')
-#    echo $file"  ---->  "$new_filename
-#    mv "$file" "$new_filename"
-#    echo $file" renamed"
+    echo
     mv "$file" "$PLAYLISTS_DIR/$playlist"
-    echo "file "$file" has been succesfully moved to the selected playlist $playlist inside the default playlist directory $PLAYLISTS_DIR."
+    echo "file $file has been succesfully moved to the playlist: $playlist inside the playlist directory $PLAYLISTS_DIR."
 done
 
-# Exit the script printing files moved
-echo;
+# round-up
+echo
 echo "Operation successful. Files moved: "
 
-# shellcheck disable=SC2068
 for file in ${file_array[@]}
 do
-    # shellcheck disable=SC2086
     echo $file
 done
--- bulk-song-renamer.sh
+++ bulk-song-renamer.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# TODO:
-# There is no functionality to specify the renaming pattern.
-# The first to words from the filename will be removed.
-
+# old comments. the direction of the script was to rename whatever to whatever but the direction has changed. only to rename music.
+echo "bulk rename songs to remove the 'spotifydown.com - ' part"
 
 # read the files to rename
-read -rep "Files to rename: " -i '*' files_string
+read -rep "Song files to rename: " -i '*' files_string
 
 # Create an array from the input string
 readarray -t file_array <<< $files_string
@@ -16,28 +14,30 @@ echo "Files to bulk rename:"
 # shellcheck disable=SC2068
 for file in ${file_array[@]}
 do
-    echo $file
+    new_filename="${file//"spotifydown.com - "/""/}"
+    echo $file"  ---->  "$new_filename
 done
 
+# double check confirmation
 while true; do
     read -p "Start the renaming proccess for the aforementioned files? (y/n)" yn
     case $yn in
         [Yy]*) echo "Renaming start... "; break;;
-        [Nn]*) echo "Renaming cancelled. Exiting..."; exit;;
+        [Nn]*) echo "Renaming cancelled. Aborting."; exit;;
             *) echo "(y/n)";;
     esac
 done
 
 # start the renaming proccess
-# shellcheck disable=SC2068
 for file in ${file_array[@]}
 do
     echo;
-    new_filename=$(echo $file | sed 's/^[^ ]* [^ ]* //')
+    new_filename="${file//"spotifydown.com - "/""/}"
     echo $file"  ---->  "$new_filename
     mv "$file" "$new_filename"
     echo $file" renamed"
 done
 
+# print out the renamed files
 echo;
 ls -l
