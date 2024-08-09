d7cad3572bfc5152b788adba96d90bfedfcc856c
piker <piker.joestar98988@gmail.com>
2024-08-09 12:33:09 +0200

    half done run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,35 @@
 #! /bin/bash
+
+while True; do
+  read -rep "Would you like for this script to be run inside a python virtual environment? [y/N] " YNvenv
+  case $YNvenv in
+    ["Yy"]*)
+      read -rep "Where would you like for your environment to be located?\n> " venvdir
+      echo "Creating virtual environment at $venvdir ..."
+      python -m venv "$venvdir"
+      echo "Sourcing virtual environment..."
+      source "$venvdir"
+      echo "Installing dependencies locally... "
+      pip install -r ./util-downloader/dependencies.txt || echo "Error installing dependencies. Check your python installation."; exit
+      ;;
+    ["Nn"]*)
+      echo "Installing dependencies globally... "
+      pip install -r ./util-downloader/dependencies.txt || echo "Error while installing dependencies. Check your python installation."; exit
+      ;;
+    *)
+      echo;;
+  esac
+done
+
+# running downloader -> wish you luck!!
+
+
+
+
+
+
+
+
+# exit 0 -> cancelled by user
+# exit 1 -> run successfully
+# exit 2 -> error renaming
---
+++ util-downloader/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pikers-spotifydown-scraper",
+    version='0.0.1',
+    author='piker',
+    description='spotifydown.com web scraper that lets you interact with website via a cli',
+    packages=find_packages(),
+    install_requires=[
+        "loguru",
+        "playwright"
+    ],
+    entry_points={
+        'console_scripts': [
+            "util-downloader = pikers-spotifydown-scraper.__init__:run"
+        ]
+    }
+
+)
